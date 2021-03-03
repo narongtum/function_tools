6d47ecfaa29724304235ed60cf156e078b701069
narongtum <narongtum@trueaxiongames.com>
2021-03-03 09:09:20 +0700

    add tools

--- dateTimeFormat.py
+++ dateTimeFormat.py
@@ -2,11 +2,18 @@
 import datetime
 import pyperclip
 
+# Note
+# add year 
 
 # install pyper here
 # C:\Python27\Scripts>pip.exe install pyperclip
 
 date = datetime.datetime.now()
-currentDate = date.strftime('%m.%b.%d.%a_')
+currentDate = date.strftime('%y.%m.%b.%d.%a_')
 print (currentDate)
 pyperclip.copy(currentDate)
+
+
+
+
+
